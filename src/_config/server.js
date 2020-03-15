4e354e980eaa0faf4ffae3abc54e8205ec796c5b
marcelo-borges <marcelo.ro.borges@gmail.com>
2020-03-15 16:34:42 -0300

    Melhorias da api

--- src/_config/server.js
+++ src/_config/server.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const applyRoutes = require("./routes");
 const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
 const app = new Koa();
 const router = new Router();
@@ -9,6 +10,7 @@ const router = new Router();
 module.exports = () => {
   applyRoutes(router);
   app
+    .use(cors())
     .use(bodyParser())
     .use(router.routes())
     .use(router.allowedMethods());
